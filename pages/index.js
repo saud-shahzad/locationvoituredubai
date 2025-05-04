8c06ed2bf5bbb10d0b5fb5da3d273a4dd7a47056
saud-shahzad <57951076+saud-shahzad@users.noreply.github.com>
2025-05-04 10:07:28 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
   {/* Land Cruiser Card */}
   <div className="car-card">
     <img
-      src="/land-cruiser-rental-dubai.png"
+      src="/public/land-cruiser-rental-dubai.png"
       alt="Land Cruiser Rental Dubai"
       className="car-image"
     />
